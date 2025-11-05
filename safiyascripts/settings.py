57c9cd60c7d1ee7e74b0df97756fb68aa68fbd31
Truveron <ingeniouspython@gmail.com>
2025-11-05 21:26:18 +0100

    Add Counsellor’s Desk section and style updates

--- safiyascripts/settings.py
+++ safiyascripts/settings.py
@@ -130,15 +130,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import os
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
