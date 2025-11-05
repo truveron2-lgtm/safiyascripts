7262814167e225a1a8dfbc243763d03b243971c0
Truveron <ingeniouspython@gmail.com>
2025-11-05 14:18:17 +0100

    Configured DATABASE_URL for Render

--- safiyascripts/settings.py
+++ safiyascripts/settings.py
@@ -130,11 +130,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
